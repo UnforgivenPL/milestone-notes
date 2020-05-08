41704a1138c2d830007183511eaacda0ccab6fc5
miki <sorrow@unforgiven.pl>
2020-05-08 23:20:45 +0300

    #4 looking for the lost files

--- milestone-notes.sh
+++ milestone-notes.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
 echo "milestone-notes - (c) 2020 Unforgiven.pl - written by Miki Olsz - Apache 2.0 License"
+set -o xtrace
+pwd
+dir
+dir /
 
-ruby /milestone-notes.rb $*
+ruby milestone-notes.rb $*
+
+set +o xtrace
