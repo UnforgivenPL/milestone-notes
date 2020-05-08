abdbc9a535cc682eb7ef250c90716a493ad17a26
miki <sorrow@unforgiven.pl>
2020-05-08 23:36:52 +0300

    #4 renumbered attributes

--- milestone-notes.sh
+++ milestone-notes.sh
@@ -2,5 +2,5 @@
 
 echo "milestone-notes - (c) 2020 Unforgiven.pl - written by Miki Olsz - Apache 2.0 License"
 set -x
-ls -l /
-ruby /milestone-notes.rb "$0" "$1" "$2" "$3" "$4"
+ls -l /github/workspace
+ruby /milestone-notes.rb "$1" "$2" "$3" "$4" "$5"
