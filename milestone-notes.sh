6d850c4cfb3d2a7e1d85707a2fa1db41bf47feb5
miki <sorrow@unforgiven.pl>
2020-05-08 23:34:04 +0300

    #4 making sure ruby file is there

--- milestone-notes.sh
+++ milestone-notes.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 echo "milestone-notes - (c) 2020 Unforgiven.pl - written by Miki Olsz - Apache 2.0 License"
-set +o xtrace
-ls -l
-ruby ./milestone-notes.rb "$0" "$1" "$2" "$3" "$4"
+set -x
+ls -l /
+ruby /milestone-notes.rb "$0" "$1" "$2" "$3" "$4"
