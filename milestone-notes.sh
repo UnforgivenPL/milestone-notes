1e07cac5ae043bc1dee35127acee34f80dc735c0
miki <sorrow@unforgiven.pl>
2020-05-08 23:25:29 +0300

    #4 adding quotes to ruby exec attributes

--- milestone-notes.sh
+++ milestone-notes.sh
@@ -1,11 +1,4 @@
 #!/bin/sh
 
 echo "milestone-notes - (c) 2020 Unforgiven.pl - written by Miki Olsz - Apache 2.0 License"
-set -o xtrace
-pwd
-dir
-dir /
-
-ruby milestone-notes.rb $*
-
-set +o xtrace
+ruby milestone-notes.rb "$0" "$1" "$2" "$3" "$4"
